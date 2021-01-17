75f45b9b7cbc5b7834e18abbbd72425ec1f2558a
Nicolas Mori <nicolas.mori@outlook.de>
2021-01-17 12:42:40 +0100

    Added basic commandline args handling and load files

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,20 @@
 module Main where
 
+import System.Environment
+import System.Exit
+
 import Lib
 
 main :: IO ()
-main = someFunc
+main = do
+    contents <- getArgs >>= parse
+    putStrLn $ "Successfully loaded " ++ (show $ length contents) ++ " files."
+
+parse :: [FilePath] -> IO [String]
+parse ["-h"] = usage   >> exit
+parse ["-v"] = version >> exit
+parse fs     = mapM readFile fs
+
+usage   = putStrLn "Usage: vtt-contverter [-vh] [file1 ...]"
+version = putStrLn "vtt-converter 0.1 - Haskell Implementation"
+exit    = exitWith ExitSuccess
