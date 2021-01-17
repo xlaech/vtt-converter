d3439fff8e5c907e1d76d641a13153ba5aa3d929
Nicolas Mori <nicolas.mori@outlook.de>
2021-01-17 13:34:00 +0100

    First implementation of datatypes

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,23 @@
 module Lib
-    ( someFunc
+    ( VttFile
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Data.Map
+
+data VttFile          = VttFile VttHeader 
+                                (Map VttSectionId VttSection)
+data VttHeader        = VttHeader [VttNote]
+data VttSection       = VttSection VttTiming 
+                                   [VttPosition]
+                                   [VttSubtitleLine]
+data VttPosition      = VttPosition VttPositionKey VttPositionKey VttPositionValue
+data VttSubtitleLine  = VttSubtitleLine [VttSubtitleEntry]
+data VttSubtitleEntry = VttSubText String
+                      | VttContainer VttContainerType [VttSubtitleEntry]
+
+type VttTiming        = String
+type VttSectionId     = Int
+type VttContainerType = String
+type VttPositionKey   = String
+type VttPositionValue = String
+type VttNote          = String
